68d37307d01ccde59101ec78aed3a7cf1a23e790
김은서 <a55208806@gmail.com>
2023-09-06 20:37:33 +0900

    보스, 보스패턴 스크립트 추가

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.ComponentModel;
+
+public abstract class Boss : MonoBehaviour
+{
+	#region PublicVariables
+	public string bossName;
+	#endregion
+
+	#region PrivateVariables
+	protected Animator anim;
+	private GameObject rend;
+	private Sequence hitSeq;
+
+	[SerializeField] private Vector2 respawnPoint;
+	[SerializeField] protected int hpCurrent;
+	[SerializeField] protected int hpMax;
+	[SerializeField] protected BossPattern startPattern;
+	[SerializeField] protected List<BossPattern> patternList = new List<BossPattern>();
+
+	[SerializeField] protected int patternIndex;
+	[SerializeField] protected BossPattern currentPattern;
+	#endregion
+
+	#region PublicMethod
+	public Animator GetAnimator() => anim;
+	public int GetMaxHp() => hpMax;
+	public virtual void Initialize()
+	{
+		hpCurrent = hpMax;
+		patternIndex = -1;
+	}
+	public virtual void Hit(int _damage, GameObject _source)
+	{
+
+		hitSeq.Restart();
+		hpCurrent = Mathf.Clamp(hpCurrent - _damage, 0, hpMax);
+	}
+	public virtual void BossKilled()
+	{
+		anim.Play("Die");
+		ShutdownAction();
+	}
+	public void PatternStart()
+	{
+		Debug.Log("start");
+		currentPattern = startPattern;
+		currentPattern.StartAct();
+	}
+	public void PatternNext()
+	{
+		patternIndex = GetNextPatternIndex(patternIndex);
+		currentPattern = patternList[patternIndex];
+		currentPattern.Act();
+	}
+	public void ShutdownAction()
+	{
+		currentPattern.ShutdownAction();
+	}
+	#endregion
+
+	#region PrivateMethod
+	protected virtual void Awake()
+	{
+		//rend = transform.Find("renderer").gameObject;
+		//transform.Find("renderer").TryGetComponent(out anim);
+	}
+	protected virtual void OnEnable()
+	{
+		transform.position = respawnPoint;
+	}
+	protected virtual void Start()
+	{
+		/*hitSeq = DOTween.Sequence()
+			.SetAutoKill(false)
+			.Append(rend.transform.DOShakePosition(0.1f, 0.2f))
+			.Pause();*/
+		Initialize();
+	}
+	private int GetNextPatternIndex(int _currentIndex)
+	{
+		int result = _currentIndex;
+		if(result >= patternList.Count - 1)
+		{
+			result = 0;
+		}
+		else
+		{
+			++result;
+		}
+		return result;
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/BossPattern.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public abstract class BossPattern : MonoBehaviour
+{
+	#region PublicVariables
+	#endregion
+
+	#region PrivateVariables
+	private Boss main;
+	protected Animator anim;
+	[SerializeField] protected string animationStateName;
+	[SerializeField] protected int preDelaySeconds;
+	[SerializeField] protected int postDelaySeconds;
+	private WaitForSeconds waitpreDelay;
+	private WaitForSeconds waitpostDelay;
+	protected CancellationTokenSource preDelaySource = new CancellationTokenSource();
+	protected CancellationTokenSource postDelaySource = new CancellationTokenSource();
+	#endregion
+
+	#region PublicMethod
+
+	public void StartAct()
+    {
+		StartCoroutine(nameof(Act));
+    }
+	public IEnumerator Act()
+	{
+		PreProcessing();
+		//PlayAnimation();
+		Debug.Log("delay");
+		yield return waitpreDelay;
+		ActionContext();
+		yield return waitpostDelay;
+		PostProcessing();
+
+		CallNextAction();
+	}
+	public void CallNextAction()
+	{
+		main.PatternNext();
+	}
+	public void ShutdownAction()
+	{
+		preDelaySource.Cancel();
+		postDelaySource.Cancel();
+	}
+	#endregion
+
+	#region PrivateMethod
+	protected virtual void Awake()
+	{
+		TryGetComponent(out main);
+	}
+	private void Start()
+	{
+		anim = main.GetAnimator();
+		waitpreDelay = new WaitForSeconds(preDelaySeconds);
+		waitpostDelay = new WaitForSeconds(postDelaySeconds);
+	}
+	protected virtual void OnEnable()
+	{
+		if (preDelaySource != null)
+			preDelaySource.Dispose();
+		preDelaySource = new CancellationTokenSource();
+		if(postDelaySource != null)
+			postDelaySource.Dispose();
+		postDelaySource = new CancellationTokenSource();
+	}
+	protected virtual void OnDisable()
+	{
+		ShutdownAction();
+	}
+	private void PlayAnimation()
+	{
+		if (animationStateName != "")
+		{
+			transform.Find("renderer").TryGetComponent(out anim);
+			anim.Play(animationStateName);
+		}
+	}
+	protected virtual void PreProcessing()
+	{
+
+	}
+	protected virtual void PostProcessing()
+	{
+
+	}
+	protected abstract void ActionContext();
+	#endregion
+}
