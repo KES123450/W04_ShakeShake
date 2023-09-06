f54eb38f394cbe18b91402b8b36dfe9992928f92
손명진 <sonmyungzin1@gmail.com>
2023-09-06 20:34:41 +0900

    (+) PlayerController, PlayerMove 스크립트 추가

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Move")]
+    [SerializeField] float speed;
+    [Header("Roll")]
+    [SerializeField] float rollDistance;
+    [SerializeField] float rollDuration;
+
+    PlayerController player;
+    Rigidbody2D playerRigidbody;
+    Vector2 moveDirection;
+    Vector2 rollDirection;
+
+    public bool CanMove { get; set; }
+    public bool IsRollEnded { get; private set; }
+    bool IsRolling { get { return player.CurrentState == PlayerState.Roll; } }
+
+    float rollStartTime;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerController>();
+        playerRigidbody = GetComponent<Rigidbody2D>();    
+    }
+    private void Start()
+    {
+        IsRollEnded = false;
+    }
+    private void FixedUpdate()
+    {
+        if (!CanMove)
+        {
+            return;
+        }
+
+        switch (player.CurrentState)
+        {
+            case PlayerState.Idle: FixedUpdateIdle(); break;
+            case PlayerState.Roll: FixedUpdateRoll(); break;
+            case PlayerState.Action: FixedUpdateAction(); break;
+            case PlayerState.Death: FixedUpdateDeath(); break;
+        }
+    }
+    void FixedUpdateIdle()
+    {
+        playerRigidbody.velocity = moveDirection * speed;
+    }
+    void FixedUpdateRoll()
+    {
+        if (Time.time > rollStartTime + rollDuration)
+        {
+            IsRollEnded = true;
+        }
+    }
+    void FixedUpdateAction()
+    {
+
+    }
+    void FixedUpdateDeath()
+    {
+    }
+
+    public void SetDireciton(Vector2 direction)
+    {
+        moveDirection = direction;
+    }
+
+    public void StartRoll()
+    {
+        rollStartTime = Time.time;
+        rollDirection = moveDirection;
+
+        var rollSpeed = rollDistance / rollDuration;
+        playerRigidbody.velocity = rollDirection * (rollSpeed);
+    }
+    public void EndRoll()
+    {
+        IsRollEnded = false;
+    }
+
+}
