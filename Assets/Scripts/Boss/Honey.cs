10dd93b95c182b49eac0f152a1ee4d30e89a99e0
김은서 <a55208806@gmail.com>
2023-09-08 14:48:29 +0900

    보스 허니 패턴 추가

--- Assets/Scripts/Boss/Honey.cs
+++ Assets/Scripts/Boss/Honey.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Honey : MonoBehaviour
 {
+    [SerializeField] private float defaultSpeed;
+    [SerializeField] private float downSpeed;
+    [SerializeField] private float honeyTime;
 
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(nameof(SpeedDown), collision.GetComponent<PlayerMove>());
+        }
+    }
+
+    private IEnumerator SpeedDown(PlayerMove player)
+    {
+        player.SetSpeedModifier(downSpeed);
+        yield return new WaitForSeconds(honeyTime);
+        player.SetSpeedModifier(defaultSpeed);
+    }
 }
