a276f3a07f618f9773ebfde74479e0ba470e793b
김은서 <a55208806@gmail.com>
2023-09-08 20:03:52 +0900

    보스 감자 패턴 시작

---
+++ Assets/Scripts/Boss/Potato.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potato : MonoBehaviour
+{
+    private Vector2 direction;
+
+
+    private void MoveForward()
+    {
+
+    }                                                                                                                                                                               
+}
