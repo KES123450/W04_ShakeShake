c292a55dd5025ea08ff65509e7c6141242e77270
김은서 <a55208806@gmail.com>
2023-09-08 19:48:46 +0900

    보스의 호박 패턴 완성

---
+++ Assets/Scripts/Boss/Pumpkin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour,IDamageable
+{
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+    private Collider2D collider;
+
+    void Start()
+    {
+        collider= GetComponent<Collider2D>();
+    }
+    public void OnDamage(int damage = 1)
+    {
+        Destroy(gameObject);
+    }
+
+    public void EnableCollider()
+    {
+        collider.enabled = true;
+        Collider2D col = Physics2D.OverlapCircle((Vector2)transform.position, radius,playerLayer);
+        if (col != null)
+        {
+            // 플레이어의 OnDamage() 호출
+        }
+    }
+
+}
