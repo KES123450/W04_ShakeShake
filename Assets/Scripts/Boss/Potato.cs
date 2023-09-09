121b5de7a03810fb7afc5fa24708bae8ad86af22
김은서 <a55208806@gmail.com>
2023-09-09 12:04:11 +0900

    보스의 감자 패턴 완성

--- Assets/Scripts/Boss/Potato.cs
+++ Assets/Scripts/Boss/Potato.cs
@@ -2,13 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Potato : MonoBehaviour
+public class Potato : MonoBehaviour, IDamageable
 {
-    private Vector2 direction;
+    [SerializeField] private LayerMask potatoLayer;
+    [SerializeField] private float raycastDistance;
+    [SerializeField] private float moveSpeed;
+    private bool isMove=false;
+    private Vector2 direction=Vector2.right;
 
 
+    public void InitPotato(Vector2 dir, float speed)
+    {
+        direction = dir;
+        moveSpeed = speed;
+    }
+
+    public void OnDamage(int damage = 1)
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void CanMove() => isMove = true;
     private void MoveForward()
     {
+        if (isMove)
+        {
+            transform.Translate(direction * moveSpeed);
 
-    }                                                                                                                                                                               
+            if (RaycastCollision())
+            {
+                // 뛰는 애니메이션 실행
+                return;
+            }
+        }
+        
+    }
+    
+    private bool RaycastCollision()
+    {
+        Vector2 playerPos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(playerPos, Vector2.right, raycastDistance, potatoLayer);
+
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        MoveForward();
+    }
 }
