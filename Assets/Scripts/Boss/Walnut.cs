174e5279aa767c2f0e8f711a7036f65f1d17eebb
손명진 <sonmyungzin1@gmail.com>
2023-09-09 11:32:49 +0900

    (+) 호두 패턴 추가

---
+++ Assets/Scripts/Boss/Walnut.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walnut : MonoBehaviour
+{
+    float traceRadius;
+    float angularSpeed;
+    
+    public void Initialize(Vector2 origin, float radius, float angleRadian)
+    {
+        traceRadius = radius;
+        SetLocalPosition(origin, angleRadian);
+    }
+    public void UpdateRotate(Vector2 origin, float angleRadian)
+    {
+        SetLocalPosition(origin, angleRadian);
+    }
+    public void EndRotate()
+    {
+        Destroy(gameObject);
+    }
+
+    void SetLocalPosition(Vector2 origin, float radian)
+    {
+        var direction = Vector2.right * Mathf.Sin(radian) + Vector2.up * Mathf.Cos(radian);
+        transform.position = origin + direction * traceRadius;
+    }
+}
---
+++ Assets/Scripts/Boss/WalnutPattern.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WalnutPattern : BossPattern
+{
+    [SerializeField] int walnutNum;
+    [SerializeField] float walnutRadius;
+    [SerializeField] float walnutAngularSpeed;
+    [SerializeField] GameObject shadowPrefab;
+    [SerializeField] GameObject walnutPrefab;
+
+    float currentAngle;
+    GameObject shadow;
+    List<Walnut> spawnedWalnuts = new();
+
+    private void Update()
+    {
+        if (spawnedWalnuts.Count > 0)
+        {
+            currentAngle += walnutAngularSpeed * Time.deltaTime;
+            ForeachWalnut((index, radian) =>
+            {
+                spawnedWalnuts[index].UpdateRotate(transform.position, radian + currentAngle * Mathf.Deg2Rad);
+            });
+        }
+    }
+
+    protected override void PreProcessing()
+    {
+        spawnedWalnuts.Clear();
+        base.PreProcessing();
+        CastShadow();
+    }
+    protected override void ActionContext()
+    {
+        SpawnWalnuts();
+    }
+    protected override void PostProcessing()
+    {
+        base.PostProcessing();
+        foreach (var w in spawnedWalnuts.ToList())
+        {
+            w.EndRotate();
+        }
+    }
+    void CastShadow()
+    {
+        shadow = Instantiate(shadowPrefab, transform.position, Quaternion.identity);
+        shadow.transform.localScale = Vector3.one * walnutRadius * 2;
+        Destroy(shadow, preDelaySeconds);
+    }
+    void SpawnWalnuts()
+    {
+        ForeachWalnut((index, radian) =>
+        {
+            var walnut = Instantiate(walnutPrefab, transform.position, Quaternion.identity).GetComponent<Walnut>();
+            walnut.Initialize(transform.position, walnutRadius, radian);
+            spawnedWalnuts.Add(walnut);
+        });
+    }
+
+    void ForeachWalnut(System.Action<int, float> action)
+    {
+        for (int i = 0; i < walnutNum; i++)
+        {
+            var radian = (2 * Mathf.PI * i) / walnutNum;
+            action?.Invoke(i, radian);
+        }
+    }
+}
