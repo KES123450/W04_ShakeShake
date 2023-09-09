ae5ea61bc67938264202035395c2467d395c785f
손명진 <sonmyungzin1@gmail.com>
2023-09-09 22:04:46 +0900

    (+) 플레이어 캐릭터 무적타임 추가

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,29 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] Color rollColor;
     [SerializeField] Color actionColor;
     [SerializeField] Color deathColor;
+    [Header("Invunerable Color")]
+    [SerializeField] Color invunerableColor;
+    [SerializeField] int invunerableBlinkNum;
+    [SerializeField] Ease invunerableBlinkEaseFunction;
 
     PlayerController player;
     SpriteRenderer spriteRenderer;
+    Animator animator;
 
+    
     PlayerState previousState;
     Color defaultColor;
+    bool isInvunerable;
+    float invunerableDuration;
+    float invunerableTimer;
+
 
     void Awake()
     {
         player = GetComponent<PlayerController>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
         defaultColor = spriteRenderer.color;
         previousState = player.CurrentState;
+        isInvunerable = false;
     }
     void Update()
     {
@@ -33,6 +46,10 @@ public class PlayerAnimation : MonoBehaviour
             case PlayerState.Roll: UpdateRoll(); break;
             case PlayerState.Action: UpdateAction(); break;
             case PlayerState.Death: UpdateDeath(); break;
+        }
+        if (isInvunerable)
+        {
+            UpdateInvunerableColor();
         }
     }
 
@@ -55,6 +72,7 @@ public class PlayerAnimation : MonoBehaviour
 
             //temp code
             spriteRenderer.color = rollColor;
+
         }
     }
     void UpdateAction()
@@ -76,5 +94,31 @@ public class PlayerAnimation : MonoBehaviour
             //temp code
             spriteRenderer.color = deathColor;
         }
+    }
+
+    public void OnStartInvunerable(float effectDuration)
+    {
+        isInvunerable = true;
+        invunerableDuration = effectDuration;
+        invunerableTimer = invunerableDuration;
+    }
+    public void OnEndInvunerable()
+    {
+        isInvunerable = false;
+        spriteRenderer.color = defaultColor;
+    }
+    public void UpdateInvunerableColor()
+    {
+        invunerableTimer -= Time.deltaTime;
+
+        var blinkDuration = invunerableDuration / invunerableBlinkNum;
+        var interpolatedValue = ((invunerableDuration - invunerableTimer) % blinkDuration) / blinkDuration;
+        if (interpolatedValue > 0.5f)
+        {
+            interpolatedValue = 1 - interpolatedValue;
+        }
+        var easedValue = DOVirtual.EasedValue(0, 1, interpolatedValue * 2, invunerableBlinkEaseFunction);
+        spriteRenderer.color = Color.Lerp(defaultColor, invunerableColor, easedValue);
+
     }
 }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] int maxHealth;
-
+    [SerializeField] float invunerableDuration;
     PlayerController player;
+    PlayerAnimation playerAnimation;
 
     int _currentHealth;
+    float invunerableTimer;
+
 
     public int CurrentHealth
     {
@@ -16,6 +19,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         set { _currentHealth = Mathf.Clamp(value, 0, maxHealth); }
     }
     public bool IsAlive => _currentHealth > 0;
+    public bool IsInvunerable => invunerableTimer > 0;
     
     void OnGUI()
     {
@@ -28,16 +32,43 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     void Awake()
     {
         player = GetComponent<PlayerController>();
+        playerAnimation = GetComponent<PlayerAnimation>();
     }
     void Start()
     {
         CurrentHealth = maxHealth;
     }
+    void Update()
+    {
+        if (IsAlive && IsInvunerable)
+        {
+            invunerableTimer -= Time.deltaTime;
+            if (!IsInvunerable)
+            {
+                EndInvunerable();
+            }
+        }
+    }
     public void OnDamage(int value = 1)
     {
-        if (IsAlive)
+        if (IsAlive && !IsInvunerable)
         {
             CurrentHealth -= value;
+            if (IsAlive)
+            {
+                StartInvunerable();
+            }
         }
+    }
+
+    void StartInvunerable()
+    {
+        invunerableTimer = invunerableDuration;
+        playerAnimation.OnStartInvunerable(invunerableDuration);
+    }
+
+    void EndInvunerable()
+    {
+        playerAnimation.OnEndInvunerable();
     }
 }
