43f3a6d7bd116c52c7b9024908e30b12db9df5c9
김은서 <a55208806@gmail.com>
2023-09-11 10:56:33 +0900

    허니 패턴 수정
    - 허니 시간이 지나면 천천히 사라지도록 수정
    - 콜라 맞으면 좀더 페이드 아웃 빠르게 되도록 수정

--- Assets/Scripts/Boss/Honey.cs
+++ Assets/Scripts/Boss/Honey.cs
@@ -2,24 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Honey : MonoBehaviour
+public class Honey : MonoBehaviour,IDamageable
 {
+    [SerializeField] private float duration;
+    private float nowduration;
+    [SerializeField] private float damage;
+    private SpriteRenderer honeySprtie;
     [SerializeField] private float defaultSpeed;
     [SerializeField] private float downSpeed;
     [SerializeField] private float honeyTime;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+        honeySprtie = GetComponent<SpriteRenderer>();
+        nowduration = duration;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            StartCoroutine(nameof(SpeedDown), collision.GetComponent<PlayerMove>());
+            SetDownSpeed(collision.GetComponent<PlayerMove>());
         }
     }
 
-    private IEnumerator SpeedDown(PlayerMove player)
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SetDefalutSpeed(collision.GetComponent<PlayerMove>());
+        }
+    }
+
+    private void SetDownSpeed(PlayerMove player)
     {
         player.SetSpeedModifier(downSpeed);
-        yield return new WaitForSeconds(honeyTime);
+    }
+
+    private void SetDefalutSpeed(PlayerMove player)
+    {
         player.SetSpeedModifier(defaultSpeed);
+    }
+    private void FadeOutHoney()
+    {
+        if (nowduration <= 0)
+        {
+            DestroyObject();
+        }
+        nowduration -= Time.deltaTime;
+        float nowAlpha = Mathf.Lerp(0, 1, nowduration / duration);
+        Color alpha = Color.white;
+        alpha.a = nowAlpha;
+        honeySprtie.color = alpha;
+    }
+
+    private void DestroyObject()
+    {
+
+        Destroy(gameObject);
+    }
+    private void Update()
+    {
+        FadeOutHoney();
+    }
+
+    public void OnDamage(int damage = 1)
+    {
+        nowduration -= damage;
     }
 }
